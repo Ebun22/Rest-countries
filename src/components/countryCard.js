187fe8c59d7e8893a59116599ff7183aa0748263
Ebun22 <ebundavid7@gmail.com>
2021-08-05 17:43:35 +0100

    Updated commit

--- src/components/countryCard.js
+++ src/components/countryCard.js
@@ -4,12 +4,12 @@ class Card extends Component {
   render(){
     const { data, click } = this.props
       return(
-        <div onClick={click} class="rounded overflow-hidden shadow-2xl bg-gray-700">
-          <div class="w-full h-48">
+        <div onClick={click} class="rounded overflow-hidden shadow-2xl bg-gray-700 mb-12 md:m-0 mx-12" >
+          <div class="w-full md:h-48">
             <img style={{objectFit: "cover",  height: "100%", width: "100%"}} src={data.flag} alt="flag" />
           </div>
-          <div className="details sm:p-7">
-            <p class="sm:text-lg font-bold sm:pb-4">{data.name}</p>
+          <div className="details p-7">
+            <p class="md:text-lg font-bold md:pb-4">{data.name}</p>
             <p><span class="font-medium">Population: </span><span class="font-hairline">{data.population}</span></p>
             <p><span class="font-medium">Region: </span><span class="font-hairline">{data.region}</span></p>
             <p><span class="font-medium">Capital: </span><span class="font-hairline">{data.capital}</span></p>
