6d0511a0863cb6f78303fb1b448acf163e91c6f6
Ebun22 <ebundavid7@gmail.com>
2021-08-05 17:34:39 +0100

    Updated commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,7 +6,8 @@ class Search extends Component {
     return (
         <input type="search"  value={query} onChange={change} placeholder="Search for a country"
         style={{boxShadow:" 5px 5px 5px 0px hsla(0, 0%, 0%, 0.1)"}}
-        class="my-7 py-2 px-20 bg-gray-700 focus:outline-none rounded-md placeholder-white placeholder-opacity-25 col-span-3 justify-self-start"
+        className="row-start-1 col-start-1"
+        class="my-7 md:mx-0 mx-4 py-2 sm:px-20 bg-gray-700 focus:outline-none rounded-md placeholder-white placeholder-opacity-25 md:col-span-3 sm:col-span-2  md:justify-self-start"
         />
       )
   }
