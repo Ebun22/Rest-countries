0bb8a0d005e802de0ba1408b952603cd5b458957
Ebun22 <ebundavid7@gmail.com>
2021-08-05 17:33:51 +0100

    Updated commit

--- src/components/countries.js
+++ src/components/countries.js
@@ -12,9 +12,9 @@ const Countries=({ country, name, region , handleSearch, handleClick, handleFilt
       )
     })
     return (
-      <div class="bg-gray-800 px-16 grid gap-9">
-        <Search change={handleSearch} />
-        <nav class="row-start-1 col-start-4 my-7 justify-self-end">
+      <div class="md:bg-blue-800 sm:bg-red-800 bg-gray-800 md:px-16 mx-4 md:mx-0 px-0 md:grid md:gap-9 sm:gap-4">
+        <Search change={handleSearch}/>
+        <nav class="row-start-1 col-start-4 md:my-7 my-10 mt-2 mx-4 md:mx-0 justify-self-end">
           <button class="bg-gray-700 py-2 px-4 rounded text-sm pr-8">Filter by Region</button>
           {dropDown ? <Filter region={handleFilter} dropDown={dropDown}/> : " "}
         </nav>
